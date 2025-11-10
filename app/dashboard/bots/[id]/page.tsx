0fce522346a3d545c33aaa0823bdbc119785a3ae
Amit Chawla <129105171+amitstar69@users.noreply.github.com>
2025-11-10 11:45:28 -0500

    Clarify 'use client' usage in page.tsx
    Added a comment to clarify usage of 'use client'.

--- app/dashboard/bots/[id]/page.tsx
+++ app/dashboard/bots/[id]/page.tsx
@@ -1,5 +1,6 @@
 import Client from './Client';
 
+// Do NOT add 'use client' in this file.
 export default async function Page(props: any) {
   const { id } = await (props as any).params;
   return <Client id={id} />;
